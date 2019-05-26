9993b4f766e01b77cda657ed56d73a14187490cb
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-26 23:27:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -71,16 +71,16 @@ def organize_schools(array)
   end
   return school_hash
 end
-def organize_schools(schools)
-  output = {}
-  schools.each do |key, location_hash|
-    location_hash.each do |k, location|
-      if output.include?(location)
-        output[location] = key
-      elsif output.has_key?(location)
-        output[location] << key
-      end
-    end
-  end
-  output
-end
+# def organize_schools(schools)
+#   output = {}
+#   schools.each do |key, location_hash|
+#     location_hash.each do |k, location|
+#       if output.include?(location)
+#         output[location] = key
+#       elsif output.has_key?(location)
+#         output[location] << key
+#       end
+#     end
+#   end
+#   output
+# end
